95f5c3aef88e78d23ef858235b2508694de8b20d
GitamDeveloper <143604314+GitamDeveloper@users.noreply.github.com>
2023-09-15 12:03:11 +0300

    Update accsus.lua
    2

--- accsus.lua
+++ accsus.lua
@@ -32,7 +32,7 @@ end)
 
 while wait(0.5) do
 	if nametrue == true and passwordtrue == true then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/GitamDeveloper/rhack/main/rhack_mobile_russ.lua"))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/GitamDeveloper/rhack/main/rhack_mobile_v.4.lua"))()
 		nametrue = false
 		passwordtrue = false
 	end
